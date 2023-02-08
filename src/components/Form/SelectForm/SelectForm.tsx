812529ad397a0124e8f3721527fbefde16cdfce0
Duc Quang <ducquang03102000@gmail.com>
2023-02-08 16:01:46 +0700

    UPDATE todo list

---
+++ src/components/Form/SelectForm/SelectForm.tsx
@@ -0,0 +1,82 @@
+import { Box, FormControl, FormLabel, Select, Text } from "@chakra-ui/react";
+import { FC, useCallback } from "react";
+import { UseFormRegister } from "react-hook-form";
+
+interface SelectOption {
+  label: string;
+  value: string;
+}
+
+interface Props {
+  errMessage?: string;
+  register: UseFormRegister<any>;
+  label: string;
+  isRequired?: boolean;
+  name: string;
+  displayType?: "inline" | "block";
+  options: SelectOption[];
+}
+
+const SelectForm: FC<Props> = ({
+  errMessage,
+  register,
+  label,
+  isRequired = false,
+  name,
+  displayType = "block",
+  options = [],
+  ...other
+}) => {
+  const renderRequired = useCallback(
+    (label: string) => (
+      <Box display="flex" fontSize="14px">
+        <Text color="blackAlpha.600">{label}</Text>
+        <Text color="red.400" fontWeight="bold" ml="3px">
+          *
+        </Text>
+      </Box>
+    ),
+    []
+  );
+  return (
+    <>
+      <FormControl isInvalid={!!errMessage} display="flex">
+        <FormLabel
+          htmlFor={name}
+          w={displayType === "inline" ? "160px" : "initial"}
+          display="flex"
+          alignItems="center"
+        >
+          {!isRequired ? (
+            <Text color="blackAlpha.600" fontSize="14px">
+              {label}
+            </Text>
+          ) : (
+            renderRequired(label)
+          )}
+        </FormLabel>
+        <Select
+          id={name}
+          errorBorderColor="red.400"
+          color="blackAlpha.700"
+          {...other}
+          {...register(name)}
+        >
+          {options.length > 0 &&
+            options.map((option) => (
+              <option value={option.value} key={option.value}>
+                {option.label}
+              </option>
+            ))}
+        </Select>
+      </FormControl>
+      {errMessage ? (
+        <Text fontSize="12px" fontWeight="bold" color="red.400" textAlign="end">
+          {errMessage}
+        </Text>
+      ) : null}
+    </>
+  );
+};
+
+export default SelectForm;
